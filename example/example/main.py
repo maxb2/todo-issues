1a59883d615065bc016ec6f8fb20bc83931362df
Matt Anderson <matt@manderscience.com>
2023-11-01 22:20:22 -0500

    wip

---
+++ example/example/main.py
@@ -0,0 +1,27 @@
+"""Example Repo
+
+TODO: module docstring
+"""
+
+# TODO: comment
+
+# TODO: multiline
+#       comment
+
+
+def foo():
+    """Foo example.
+
+    TODO: function docstring
+    """
+    pass
+
+# normal comment
+
+def bar():
+    """Bar.
+    """
+    pass
+
+BAZ = 1
+"""Baz."""
